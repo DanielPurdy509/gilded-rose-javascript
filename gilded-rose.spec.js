fe9e19685926293d011f15599a6f91602cf82d11
DanielPurdy509 <danielpurdy509@gmail.com>
2023-04-20 13:54:39 -0700

    Tests written for basic itmes, dexterity vest, aged brie, elixir of mongoose, and hand of ragnaros

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -1,14 +1,68 @@
 import { expect, describe, it } from "vitest";
 import { Item, items, updateQuality } from "./gilded-rose.js";
 
-describe("updateQuality", () => {
-  it("reduces quality and sellIn of basic items by 1", () => {
-    const testItem = new Item("basic", 5, 3);
-    items.push(testItem);
+// Testing dynamics of basic items -------------------------------------------------------------
+
+// describe("basicUpdateQuality", () => {
+//   it("reduces quality and sellIn of basic items by 1", () => {
+//     const testItem = new Item("basic", 5, 3);
+//     items.push(testItem);
+
+//     updateQuality();
+
+//     expect(testItem.quality).toBe(2);
+//     expect(testItem.sellIn).toBe(4);
+//   });
+// });
+
+// Testing dynamics of Dexterity Vest -------------------------------------------------
+
+// describe("vestQuality", () => {
+//   it("reduces quality and sellIn of dexterity vest by 1", () => {
+//     const testItem = items[0];
+
+//     updateQuality();
+
+//     expect(testItem.sellIn).toBe(9);
+//     expect(testItem.quality).toBe(19);
+//   });
+// });
+
+// Testing dynamics of Aged Brie -----------------------------------------------------------
+
+// describe("brieQuality", () => {
+//   it("increases quality as item gets older, but does not exceed 50 quality", () => {
+//     const testItem = items[1];
+
+//     updateQuality();
+
+//     expect(testItem.sellIn).toBe(1);
+//     expect(testItem.quality).toBe(1);
+//   });
+// });
+
+// Testing dynamics of Elixir -------------------------------------------------------------
+
+// describe("elixirQuality", () => {
+//   it("reduces quality and sellIn of Elixir of Mongoose by 1", () => {
+//     const testItem = items[2];
+
+//     updateQuality();
+
+//     expect(testItem.sellIn).toBe(4);
+//     expect(testItem.quality).toBe(6);
+//   });
+// });
+
+// Testing dynamics of Hand ----------------------------------------------------------------
+
+describe("handQuality", () => {
+  it("reduces quality and sellIn of Sulfuras, Hand of Ragnaros by 1", () => {
+    const testItem = items[3];
 
     updateQuality();
 
-    expect(testItem.quality).toBe(2);
-    expect(testItem.sellIn).toBe(4);
+    expect(testItem.sellIn).toBe(0);
+    expect(testItem.quality).toBe(80);
   });
 });
